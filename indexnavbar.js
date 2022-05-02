956db4edb55518d2f6d231f39f66319a573444cb
Shivam Gupta <100027945+shivamcool12345@users.noreply.github.com>
2022-05-03 00:02:47 +0530

    newapp

---
+++ indexnavbar.js
@@ -0,0 +1,10 @@
+
+let navbar=()=>{
+    return`
+    <h1><a href="news.html">News APP</a></h1>
+    <div id="search_input">
+      <input type="text" id="countrycode" placeholder="Search News" />
+    </div>`;
+};
+
+export { navbar };
---
+++ navbar.js
@@ -0,0 +1,9 @@
+let navbar=()=>{
+    return`
+    <h1><a href="search.html">News APP</a></h1>
+    <div id="search_input">
+      <input type="text" id="q" placeholder="Search News" />
+    </div>`;
+};
+
+export { navbar };
